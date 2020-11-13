8f5ebb6813e8cc1617df30074f4e80e3cadf9153
Acdaling <acdaling@gmail.com>
2020-11-13 12:40:31 +0100

    Updated types of the GameBacklog object

--- app/src/main/java/com/example/madlevel5task2/model/GameBacklog.kt
+++ app/src/main/java/com/example/madlevel5task2/model/GameBacklog.kt
@@ -3,15 +3,17 @@ package com.example.madlevel5task2.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.util.*
+import java.time.DayOfWeek
+import java.time.Month
+import java.time.Year
 
 @Entity(tableName = "backlogTable")
 class GameBacklog(
     @ColumnInfo(name = "title") var title: String,
-    @ColumnInfo(name = "platform") var platform: Date,
-    @ColumnInfo(name = "day") var day: Date,
-    @ColumnInfo(name = "month") var month: Date,
-    @ColumnInfo(name = "year") var year: Date,
+    @ColumnInfo(name = "platform") var platform: String,
+    @ColumnInfo(name = "day") var day: DayOfWeek,
+    @ColumnInfo(name = "month") var month: Month,
+    @ColumnInfo(name = "year") var year: Year,
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "id") var id: Long
 )
