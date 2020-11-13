e04dbf78e114a55f3623aa974036d915639dc827
Acdaling <acdaling@gmail.com>
2020-11-13 14:17:15 +0100

    Removed unused code

--- app/src/main/java/com/example/madlevel5task2/repository/GameBacklogRepository.kt
+++ app/src/main/java/com/example/madlevel5task2/repository/GameBacklogRepository.kt
@@ -5,9 +5,7 @@ import androidx.lifecycle.LiveData
 import com.example.madlevel5task2.dao.GameBacklogDao
 import com.example.madlevel5task2.database.GameBacklogRoomDatabase
 import com.example.madlevel5task2.model.GameBacklog
-import kotlinx.coroutines.InternalCoroutinesApi
 
-@InternalCoroutinesApi
 class GameBacklogRepository(context: Context) {
     private val gameBacklogDao: GameBacklogDao
 
