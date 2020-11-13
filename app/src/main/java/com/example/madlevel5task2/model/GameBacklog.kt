f092cc0237eba15c8bebdfb6308318897f8318ab
Acdaling <acdaling@gmail.com>
2020-11-13 09:45:56 +0100

    Created a Gamebacklog entity

---
+++ app/src/main/java/com/example/madlevel5task2/model/GameBacklog.kt
@@ -0,0 +1,17 @@
+package com.example.madlevel5task2.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity(tableName = "backlogTable")
+class GameBacklog(
+    @ColumnInfo(name = "title") var title: String,
+    @ColumnInfo(name = "lastUpdated") var platform: Date,
+    @ColumnInfo(name = "day") var day: Date,
+    @ColumnInfo(name = "month") var month: Date,
+    @ColumnInfo(name = "year") var year: Date,
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id") var id: Long
+)
