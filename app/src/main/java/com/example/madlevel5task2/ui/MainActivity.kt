9396317b2ab7e3d842286c5ce0a07f2a3db942e9
Acdaling <acdaling@gmail.com>
2020-11-14 20:02:20 +0100

    Able to add new item to database

--- app/src/main/java/com/example/madlevel5task2/ui/MainActivity.kt
+++ app/src/main/java/com/example/madlevel5task2/ui/MainActivity.kt
@@ -1,22 +1,25 @@
 package com.example.madlevel5task2.ui
 
+import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
 import com.example.madlevel5task2.R
 import com.example.madlevel5task2.ui.viewmodels.GameBacklogViewModel
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_second.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private val viewModel: GameBacklogViewModel by viewModels()
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -42,6 +45,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun onInitDestinationListener() {
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
@@ -52,7 +56,20 @@ class MainActivity : AppCompatActivity() {
                     fabActionScreenButton.setImageResource(android.R.drawable.ic_menu_edit)
                 }
                 R.id.SecondFragment -> {
+                    val cal = Calendar.getInstance()
+
+
                     fabActionScreenButton.setOnClickListener {
+                        cal.set(Calendar.YEAR, txtAddYear.text.toString().toInt())
+                        cal.set(Calendar.MONTH, txtAddMonth.text.toString().toInt())
+                        cal.set(Calendar.DAY_OF_MONTH, txtAddDay.text.toString().toInt())
+
+                        viewModel.insertGameBackLog(
+                            txtAddTitle.text.toString(),
+                            txtAddPlatform.text.toString(),
+                            Date.from(cal.toInstant()),
+                        )
+
                         navController.navigate(R.id.action_SecondFragment_to_FirstFragment)
                     }
                     fabActionScreenButton.setImageResource(android.R.drawable.ic_menu_save)
