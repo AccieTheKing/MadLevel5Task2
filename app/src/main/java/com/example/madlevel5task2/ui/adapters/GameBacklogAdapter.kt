dcda37c163307a2d9ffd96fce31e0ebdba047b29
Acdaling <acdaling@gmail.com>
2020-11-14 20:01:49 +0100

    Formatting the date to the proper values

--- app/src/main/java/com/example/madlevel5task2/ui/adapters/GameBacklogAdapter.kt
+++ app/src/main/java/com/example/madlevel5task2/ui/adapters/GameBacklogAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.madlevel5task2.ui.adapters
 
+import android.provider.Settings.Secure.getString
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,19 +8,31 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.madlevel5task2.R
 import com.example.madlevel5task2.model.GameBacklog
 import kotlinx.android.synthetic.main.item_card_backlog.view.*
+import java.text.DateFormatSymbols
+import java.util.*
 
 class GameBacklogAdapter(private val gameBacklogs: List<GameBacklog>) :
     RecyclerView.Adapter<GameBacklogAdapter.ViewHolder>() {
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
         fun databind(gameBacklog: GameBacklog) {
+            val calendar: Calendar = Calendar.getInstance();
+            calendar.time = gameBacklog.releaseDate
+            val dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK)
+            val monthOfYear = DateFormatSymbols().months[calendar.get(Calendar.DAY_OF_MONTH) - 1]
+            val year = calendar.get(Calendar.YEAR)
+
+
             itemView.txtViewGameTitleBacklog.text = gameBacklog.title
             itemView.txtViewPlatformTitleBacklog.text = gameBacklog.platform
-            itemView.txtViewReleaseDateBacklog.text = String().format(
-                "Release: %s %s %s", gameBacklog.day,
-                gameBacklog.month,
-                gameBacklog.year
-            )
+            itemView.txtViewReleaseDateBacklog.text =
+                String.format(
+                    "Release: %s %s %s",
+                    dayOfWeek,
+                    monthOfYear,
+                    year
+                )
         }
     }
 
